cf0675f1005bf28d127969942561a3fb057bb6ef
Ramana1122 <lovelyramanaroy@gmail.com>
2023-04-19 11:24:19 +0530

    Added

--- Day8/src/app/app.component.ts
+++ Day8/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {datafields}  from './appdata/info'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Day8';
+
+  dataitems=datafields
+
+
 }
--- Day8/src/app/app.module.ts
+++ Day8/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusinessComponent } from './business/business.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusinessComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
