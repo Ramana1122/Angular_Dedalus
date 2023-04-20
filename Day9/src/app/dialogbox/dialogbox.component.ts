bd103b8e32a162a7d44cd5bf4a65da1f2c0e688a
Ramana1122 <lovelyramanaroy@gmail.com>
2023-04-20 11:06:13 +0530

    Files Added

---
+++ Day9/src/app/dialogbox/dialogbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: './dialogbox.component.html',
+  styleUrls: ['./dialogbox.component.css']
+})
+export class DialogboxComponent {
+
+}
